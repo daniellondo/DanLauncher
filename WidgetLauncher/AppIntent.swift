e29c30028c48336f3aaf0345febad1a86ebf65d2
Daniel Londoño Ospina <37213325+daniellondo@users.noreply.github.com>
2026-09-21 14:34:44 -0500

    Support eight configurable launcher actions

--- WidgetLauncher/AppIntent.swift
+++ WidgetLauncher/AppIntent.swift
@@ -3,10 +3,16 @@ import AppIntents
 
 struct LauncherConfigurationIntent: WidgetConfigurationIntent {
     static var title: LocalizedStringResource = "Launcher"
-    static var description = IntentDescription("Choose an app or shortcut to launch.")
+    static var description = IntentDescription("Choose up to eight apps or shortcuts to launch.")
 
-    @Parameter(title: "Action")
-    var shortcut: SystemShortcut?
+    @Parameter(title: "Action 1") var shortcut1: SystemShortcut?
+    @Parameter(title: "Action 2") var shortcut2: SystemShortcut?
+    @Parameter(title: "Action 3") var shortcut3: SystemShortcut?
+    @Parameter(title: "Action 4") var shortcut4: SystemShortcut?
+    @Parameter(title: "Action 5") var shortcut5: SystemShortcut?
+    @Parameter(title: "Action 6") var shortcut6: SystemShortcut?
+    @Parameter(title: "Action 7") var shortcut7: SystemShortcut?
+    @Parameter(title: "Action 8") var shortcut8: SystemShortcut?
 
     init() {}
 }
