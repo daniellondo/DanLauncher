067dff8a94e59af9714e5276b2cce4d3f309ff77
Daniel Londoño <tmxdaniel.londonoo@kof.com>
2026-09-21 13:55:58 -0500

    Update launcher project

---
+++ WidgetLauncher/WidgetLauncherBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetLauncherBundle.swift
+//  WidgetLauncher
+//
+//  Created by Daniel Londoño Ospina on 21/09/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetLauncherBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetLauncher()
+    }
+}
